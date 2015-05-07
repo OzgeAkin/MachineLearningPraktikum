a824e4ce0f8ba1c53b839755e1a49d8b1a020c5f
Praktikant Acc041 <p041@atradig126.informatik.tu-muenchen.de>
2015-05-07 12:13:41 +0200

    dcnn classification

--- caffe/examples/imageNet_example.py
+++ caffe/examples/imageNet_example.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 #%matplotlib inline
 
 # Make sure that caffe is on the python path:
@@ -13,7 +14,8 @@ import caffe
 # and the image you would like to classify.
 MODEL_FILE = '../models/bvlc_reference_caffenet/deploy.prototxt'
 PRETRAINED = '../models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
-IMAGE_FILE = 'images/cat.jpg'
+#IMAGE_FILE = 'images/cat.jpg'
+IMAGE_FILE = '/usr/prakt/p041/OzgeAkin/MachineLearningPraktikum/week2/ex2_images/IMG_1.jpg'
 
 import os
 if not os.path.isfile(PRETRAINED):
@@ -33,14 +35,15 @@ input_image = caffe.io.load_image(IMAGE_FILE)
 
 prediction = net.predict([input_image])  # predict takes any number of images, and formats them for the Caffe net automatically
 print 'prediction shape:', prediction[0].shape
-plt.plot(prediction[0])
+#plt.plot(prediction[0])
 print 'predicted class:', prediction[0].argmax()  
 
 prediction = net.predict([input_image], oversample=False)
 print 'prediction shape:', prediction[0].shape
-plt.plot(prediction[0])
-print 'predicted class:', prediction[0].argmax()
-
+#plt.plot(prediction[0])
+predicted_class = prediction[0].argmax()
+print 'predicted class:', predicted_class
+print prediction[0][predicted_class]
 #%timeit net.predict([input_image]) 
 # Resize the image to the standard (256, 256) and oversample net input sized crops.
 input_oversampled = caffe.io.oversample([caffe.io.resize_image(input_image, net.image_dims)], net.crop_dims)
@@ -52,11 +55,32 @@ caffe_input = np.asarray([net.transformer.preprocess('data', in_) for in_ in inp
 caffe.set_mode_gpu()
 prediction = net.predict([input_image])
 print 'prediction shape:', prediction[0].shape
-plt.plot(prediction[0])
+entropy = 0
+for i in range(1000):
+	entropy = prediction[0][i]*math.log(prediction[0][i],2)
+
+print 'entropy: ', entropy
+
+#plt.plot(prediction[0])
 # Full pipeline timing.
 #%timeit net.predict([input_image])
 # Forward pass timing.
 #%timeit net.forward(data=caffe_input)
 
-    
-    
+  
+imagenet_labels_filename = caffe_root + 'data/ilsvrc12/synset_words.txt'
+try:
+    labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
+except:
+    #!../data/ilsvrc12/get_ilsvrc_aux.sh
+    labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
+top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+print labels[top_k]
+# sort top k predictions from softmax output
+#top_k = net.blobs['prob'].data[4].flatten().argsort()[-1:-6:-1]
+top_k = net.blobs['prob'].data[4][predicted_class]
+#print labels[top_k]
+print top_k
+  
+out = net.forward()
+print out['prob'][0][predicted_class]
