f96a9caa36867cd9172e44e479c29e81ebfd9dcb
OzgeAkin <akinozge0@gmail.com>
2015-05-15 00:46:32 +0200

    entropy, accuracy

--- week3/boosting.m
+++ week3/boosting.m
@@ -1,18 +1,33 @@
+clear all, close all
 train_images = loadMNISTImages('train-images.idx3-ubyte');
 train_labels = loadMNISTLabels('train-labels.idx1-ubyte');
 test_images = loadMNISTImages('t10k-images.idx3-ubyte');
 test_labels = loadMNISTLabels('t10k-labels.idx1-ubyte');
 %images(:,1:100); % the first 100 images
-Xtrain = train_images(:, 1:1000)';
-Ytrain = train_labels(1:1000);
-adaBoost = fitensemble(Xtrain, Ytrain,'AdaBoostM2',150,'Tree');
-%lpBoost = fitensemble(Xtrain, Ytrain,'LPBoost',150,'Tree');
-%totalBoost = fitensemble(Xtrain, Ytrain,'TotalBoost',150,'Tree');
-[labels,score] = predict(adaBoost,test_images');
+Xtrain = train_images(:, 1:10000)';
+Ytrain = train_labels(1:10000);
+%1000 learning cycles
+adaBoost = fitensemble(Xtrain, Ytrain,'AdaBoostM2',1000,'Tree');
+lpBoost = fitensemble(Xtrain, Ytrain,'LPBoost',1000,'Tree');
+totalBoost = fitensemble(Xtrain, Ytrain,'TotalBoost',1000,'Tree');
+[labels,score] = predict(adaBoost,test_images'); %calculate entropy with score, for loop ?
+%accuracy
+correct_labels = labels == test_labels;
+accuracy = sum(correct_labels)/length(test_labels);
+%entropy
+score_ = score(:,1) .* log2(score(:,1));
+entropy = sum(score_);
+figure(1)
 plot(loss(adaBoost,test_images',test_labels,'Mode','Cumulative'));
 xlabel('Number of trees');
 ylabel('Test classification error');
+figure(2)
+plot(resubLoss(adaBoost, 'Mode', 'Cumulative'));
 hold on
-%plot(loss(lpBoost,test_images',test_labels,'Mode','Cumulative'),'r--');
-%plot(loss(totalBoost,test_images',test_labels,'Mode','Cumulative'),'g.');
+plot(resubLoss(lpBoost,'Mode','Cumulative'),'r');
+plot(resubLoss(totalBoost,'Mode','Cumulative'),'g');
 hold off;
+legend('AdaBoostM2','LPBoost','TotalBoost','Location','NE');
+xlabel('Number of Learning Cycles');
+ylabel('Resubstitution Loss');
+
---
+++ week3/classification_validation.m
@@ -0,0 +1,34 @@
+function [correct, incorrect] = classification_validation( x1, n1, x2, n2, c )
+%CORRECT_CLASSÝFÝCATÝON_COUNT Summary of this function goes here
+%   Detailed explanation goes here
+if nargin == 5
+x_b=x1(1,:);
+y_b=x1(2,:);
+[in, on] = inpolygon(x_b, y_b, c(1,:), c(2,:));
+plot(x_b(~in),y_b(~in),'bo') % blue points that are outside
+incorrect_b = cumsum(in) + cumsum(on);
+correct_b = n1 - incorrect_b;
+    
+x_r=x2(1,:);
+y_r=x2(2,:);
+[in, on] = inpolygon(x_r, y_r, c(1,:), c(2,:));
+plot(x_r(in),y_r(in),'ro') % red points inside
+correct_r = cumsum(in) + cumsum(on);
+incorrect_r = n2 - correct_r
+
+correct = correct_r(end) + correct_b(end)
+incorrect = incorrect_b(end) + incorrect_r(end)
+end
+if nargin == 3
+x1 = x1';
+x = x1(1,:);
+y = x1(2,:);
+c = x2;
+[in, on] = inpolygon(x, y, c(1,:), c(2,:));
+%test labeled as 1
+correct = cumsum(in) + cumsum(on);
+correct = correct(end);
+incorrect = n1 - correct
+end
+end
+
